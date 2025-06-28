e4f8c37e83b47d64a49cb57b4d65980c9934ce79
Meghraj <shiradwademeghrajofficial@gmail.com>
2025-06-28 20:42:50 +0530

    First React Code

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {}, "Hello Word Namaste React");  //Create Element
+const root = ReactDOM.createRoot(document.getElementById("root")); //Create Root
+root.render(heading); //Render heading element in root
