a75c3700bd12d594acccc610b32e5dc764dbef9c
Meghraj <shiradwademeghrajofficial@gmail.com>
2025-07-01 11:40:39 +0530

    Parcel Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading = React.createElement("h1", {}, "Hello Word Namaste React");  //Create Element
+// const heading = React.createElement("h1", {id:"heading"}, "Hello Word Namaste React");  //Create Element. {} Attributes
+// const root = ReactDOM.createRoot(document.getElementById("root")); //Create Root
+// root.render(heading); //Render heading element in root
+
+import React from "react"; //Import React
+import ReactDOM from "react-dom/client"; //Import ReactDOM
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello Word Namaste React");  //Create Element. {} Attributes
 const root = ReactDOM.createRoot(document.getElementById("root")); //Create Root
 root.render(heading); //Render heading element in root
+
