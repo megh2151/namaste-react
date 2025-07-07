c4c0c3b49b38f8baa47bbdebb25b98d445615163
Meghraj <shiradwademeghrajofficial@gmail.com>
2025-07-07 07:02:04 +0530

    Episod 2 functional component

--- App.js
+++ App.js
@@ -5,7 +5,23 @@
 import React from "react"; //Import React
 import ReactDOM from "react-dom/client"; //Import ReactDOM
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello Word Namaste React");  //Create Element. {} Attributes
+
+
+const Title = () =>{
+    return (
+        <h1 id="heading">Hello Word Namaste React</h1> //Create Element
+    )
+}
+
+// Component composition. (we can use component inside component)
+const HeadingComponent = () =>(
+    <div id="Conatainer">
+        <Title />
+        <h1 className="heading">This is react functional component</h1>
+    </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root")); //Create Root
-root.render(heading); //Render heading element in root
+root.render(<HeadingComponent/>); //Render heading element in root
 
