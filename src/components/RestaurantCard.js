05f8ddfd25a0e5c3fc0b9a8efe399a0fd2056789
Meghraj <shiradwademeghraj@gmail.com>
2025-07-15 22:43:57 +0530

    Indendation

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,24 +8,24 @@ const RestaurantCard = () => {
 
     return (
         <div>
-        <button className="filterButton" onClick={() => {
-                const filteredList = restaurantList.filter(restaurant => restaurant.info.avgRating > 4.5);
-                setRestaurantList(filteredList);
-            }}>Filter Top Rated Restaurants</button>
-        <div className="res-card-container">
-            
-        {
-            restaurantList.map((restaurant) => (
-                <div key={restaurant.info.id} className="res-card">
-                    <img src={CDN_URL+restaurant.info.cloudinaryImageId} alt="Restaurant" className="res-logo" />
-                    <h2>{restaurant.info.name}</h2>
-                    <p>Rating: {restaurant.info.avgRating}</p>
-                    <p>Cuisine: {restaurant.info.cuisines.join(", ")}</p>
-                    <p>Delivery Time: {restaurant.info.sla.deliveryTime} mins</p>
-                </div>
-            ))
-        }
-        </div>
+            <button className="filterButton" onClick={() => {
+                    const filteredList = restaurantList.filter(restaurant => restaurant.info.avgRating > 4.5);
+                    setRestaurantList(filteredList);
+                }}>Filter Top Rated Restaurants</button>
+            <div className="res-card-container">
+                
+            {
+                restaurantList.map((restaurant) => (
+                    <div key={restaurant.info.id} className="res-card">
+                        <img src={CDN_URL+restaurant.info.cloudinaryImageId} alt="Restaurant" className="res-logo" />
+                        <h2>{restaurant.info.name}</h2>
+                        <p>Rating: {restaurant.info.avgRating}</p>
+                        <p>Cuisine: {restaurant.info.cuisines.join(", ")}</p>
+                        <p>Delivery Time: {restaurant.info.sla.deliveryTime} mins</p>
+                    </div>
+                ))
+            }
+            </div>
         </div>
     )
 }
